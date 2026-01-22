e94410dad1b349bbbfa4bfb89939502b40fb387c
samuel <samuel1122000@gmail.com>
2026-01-22 19:10:46 +0100

    Ajustes layout pantallas

--- src/portfoliotracker/PortfolioTracker.java
+++ src/portfoliotracker/PortfolioTracker.java
@@ -15,7 +15,7 @@ public class PortfolioTracker extends Application {
         
         primaryStage.setTitle("Portfolio Tracker");
         primaryStage.setScene(new Scene(root));
-        primaryStage.setResizable(false); // elimino el boton de maximizar
+        // primaryStage.setResizable(false); // elimino el boton de maximizar
         primaryStage.show();  
                 
     }
